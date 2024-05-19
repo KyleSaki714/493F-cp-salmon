b689cfa26285edec8ef09372794cdfc14f2572e1
KyleSaki714 <kyle13san@Gmail.com>
2024-05-19 14:55:45 -0700

    initial commit

---
+++ actor.js
@@ -0,0 +1,54 @@
+// this code adapted from Prof. Froehlich's Cookie Monster Game: https://editor.p5js.org/jonfroehlich/sketches/oUIeXC9sS
+
+const STANDSTILL_VALUE = 0.00000000000000001;
+
+class Actor extends Shape {
+  constructor(xpos, ypos, 
+              hitboxHeight, hitboxWidth, 
+              fillColor, moveSpeed) {
+    super(xpos, ypos, hitboxHeight, hitboxWidth, fillColor);
+    this.x = xpos;
+    this.y = ypos;
+    this.xvel = 0;
+    this.yvel = 0;
+    this.moveSpeed = moveSpeed;
+  }
+
+  resetPosition() {
+    this.x = 50;
+    this.y = 50;
+  }
+
+  // add val to x velocity.
+  moveX(val) {
+    this.xvel += val;
+  }
+  
+  moveY(val) {
+    this.yvel += val;
+  }
+  
+  draw() {
+    push();
+    
+    // if (this.xvel < STANDSTILL_VALUE && this.yvel < STANDSTILL_VALUE) {
+    //   this.xvel = 0;
+    //   this.yvel = 0;
+    // } else {
+      let yProgress = this.yvel / -10;
+      let xProgress = this.xvel / -10;
+
+      this.x += xProgress;
+      this.y += yProgress;
+
+      this.xvel += xProgress;
+      this.yvel += yProgress;
+    // }
+    
+    fill(this.fillColor);
+    rect(this.x, this.y, this.width, this.height);
+    // translate(this.xvel, this.yvel);
+
+    pop();
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,76 @@
+// this code adapted from Prof. Froehlich's Cookie Monster Game: https://editor.p5js.org/jonfroehlich/sketches/oUIeXC9sS
+
+const SWIMSPEED_SIDE = 0.25;
+const SWIMSPEED_UP = 0.6;
+
+class Fish extends Actor {
+  constructor(xpos, ypos, 
+              hitboxHeight, hitboxWidth, 
+              fillColor, moveSpeed) {
+    super(xpos, ypos, 
+          hitboxHeight, hitboxWidth, 
+          fillColor, moveSpeed);
+    this.swimming = false;
+    this.lastSwim = 0;
+    this.swimCooldown = 250;
+  }
+  
+  swimUpLeft() {
+    if (this.canSwim()) {
+      fish.moveX(fish.moveSpeed * SWIMSPEED_SIDE);
+      fish.moveY(fish.moveSpeed * SWIMSPEED_UP);
+      this.lastSwim = millis();
+    }
+  }
+  
+  swimUpRight() {
+    if (this.canSwim()) {
+      fish.moveX(-fish.moveSpeed * SWIMSPEED_SIDE);
+      fish.moveY(fish.moveSpeed * SWIMSPEED_UP);
+      this.lastSwim = millis();
+    }
+  }
+  
+  // prevent the fish from swimming until it rests
+  canSwim() {
+    return (millis() - this.lastSwim) > this.swimCooldown;
+  }
+}
+
+let fish;
+
+function setup() {
+  createCanvas(400, 400);
+  fish = new Fish(width / 2, height / 2, 15, 20, "#FA8072", 30);
+}
+
+function draw() {
+  background("lightblue");
+  
+  
+  if (keyIsDown(LEFT_ARROW)) {
+    fish.moveX(fish.moveSpeed);
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    fish.moveX(-fish.moveSpeed);
+  }
+  if (keyIsDown(UP_ARROW)) {
+    fish.moveY(fish.moveSpeed);
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    fish.moveY(-fish.moveSpeed);
+  }
+  
+  // "a"
+  if (keyIsDown(65)) {
+    fish.swimUpLeft();
+  }
+  // "d"
+  if (keyIsDown(68)) {
+    fish.swimUpRight();
+  }
+  
+  
+  fish.draw();
+  console.log(fish.xvel);
+}
