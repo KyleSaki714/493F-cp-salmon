a4e122b2248532bb186174edc108c5178e77b8bc
KyleSaki714 <kyle13san@Gmail.com>
2024-05-21 22:45:33 -0700

    add aruco markers positioning (beholder.js)

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
 // this code adapted from Prof. Froehlich's Cookie Monster Game: https://editor.p5js.org/jonfroehlich/sketches/oUIeXC9sS
 
+// beholder.js
+let marker0;
+let marker1;
+let _lastPosHammer; // marker 0
+let _lastPosBrush; // marker 1
+
 let backdropIm;
 let _backdrop;
 let fish;
@@ -9,20 +15,25 @@ function preload() {
 }
 
 function setup() {
-  
-  // createCanvas(1080, 720);
-  createCanvas(640, 480);
+  p5beholder.prepare();
+  createCanvas(640, 480); // small canvas so it doesnt lag
   rectMode(CENTER);
   // createCanvas(1600, 900);
   // fish = new Fish(width / 2, height * 0.90, 15, 20, "#FA8072", 10, 5);
   // fish = new Fish(0, 0, 15, 20, "#FA8072", 5, 5);
   fish = new Fish(10, color("salmon"), createVector(27, 92));
   _backdrop = new Backdrop(backdropIm);
+  _lastPosBrush = createVector(-100, -100);
+  _lastPosHammer = createVector(-100, -100);
 }
 
 
 
 function draw() {
+  // beholder updates before all code in draw()
+  marker0 = p5beholder.getMarker(0);
+  marker1 = p5beholder.getMarker(1);
+
   clear();
   background("lightblue");
   
@@ -30,24 +41,34 @@ function draw() {
 
   const scrollval = -0.5;
   
-  _backdrop.scrollX(scrollval);
-  
-  // draw background before fish for collision colors
+  _backdrop.scrollX(scrollval);  
+  // draw background before fish, for collision colors
   _backdrop.render();
 
-  // scale(2);
-  // image(backdrop, bgx, bgy);
-  // console.log(fish.pos.x);
   fish.scrollX(scrollval);
-  // if (fish.pos.x > width * 0.70) {
-  //   // bgx = bgx - ;
-  // }
-  // if (fish.pos.y > height * 0.70) {
-  //   bgy--;
-  // }
-  // console.log(color(backdrop.get(mouseX, mouseY)));
 
-  
+  // update marker positions
+  if (marker0.present) {
+    let pos = p5beholder.cameraToCanvasVector(marker0.center);
+    _lastPosHammer = pos;
+  }
+  if (marker1.present) {
+    let pos = p5beholder.cameraToCanvasVector(marker1.center);
+    _lastPosBrush = pos;
+  }
+
+  // draw sttuff with the marker positions
+  push()
+  fill("brown")
+  rect(_lastPosHammer.x, _lastPosHammer.y, 10, 10);
+  pop()
+
+  push()
+  fill("white")
+  rect(_lastPosBrush.x, _lastPosBrush.y, 20, 20);
+  pop()
+
+
   // console.log(fishCurrentColColor)
   if (fish.checkColorCollisionGrass()) {
     // console.log("Bonk");
